143043c9faf6a04501ae32fdec78041e5a14ee80
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-08-15 22:16:37 +0300

    bb

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,4 +1,4 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik,ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { nanoid } from 'nanoid';
 import { StyledForm, StyledFild, AddButton } from './StyledContactFrom'
